cadeb46421187272fdd9e844f67ff7c29b46f3b3
ThaisMA <mafalda1@protonmail.com>
2020-08-11 03:55:51 -0300

    Third Exercise

--- 3-Sum_Two_Number.py
+++ 3-Sum_Two_Number.py
@@ -8,4 +8,4 @@ number_1 = (input("Enter the first number: "))
 number_2 = (input("Enter the second number: "))
 
 sum =  float(number_1) + float(number_2)
-print('The sum of the two numbers is:  ', sum)
+print('The sum of the two numbers is:  ',sum)
