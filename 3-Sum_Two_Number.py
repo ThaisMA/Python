6fac7085dbff83a33f0809cc5673164f3dfde9a8
ThaisMA <mafalda1@protonmail.com>
2020-08-11 03:51:27 -0300

    third exercise

---
+++ 3-Sum_Two_Number.py
@@ -0,0 +1,11 @@
+#Third Exercise
+#Make a Program that asks for two numbers and prints the sum.
+
+number_1 = 0
+number_2 = 0
+
+number_1 = (input("Enter the first number: "))
+number_2 = (input("Enter the second number: "))
+
+sum =  float(number_1) + float(number_2)
+print('The sum of the two numbers is:  ', sum)
---
+++ 5-Meters_to_Centimeters.py
@@ -0,0 +1,6 @@
+#Fifth Program
+#Make a Program that converts meters to centimeters.
+
+meters = (input("Enter the value in meters: "))
+centimeters = int(meters) * 100 
+print('{0} meter(s) is {1} centimeters'.format(meters,centimeters))
