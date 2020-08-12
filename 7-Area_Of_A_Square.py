07aa992a362b694c67095be19152e712a01b60b7
ThaisMA <mafalda1@protonmail.com>
2020-08-12 02:45:01 -0300

    Seventh Exercise

--- 7-Area_Of_A_Square.py
+++ 7-Area_Of_A_Square.py
@@ -2,6 +2,5 @@
 #Make a program that calculates the area of a square, then show the double of this area to the user.
 
 side = (input("Enter the side of a square: "))
-
 square = float(side) ** 2
 print("The area of a square is: {0}".format(square))
