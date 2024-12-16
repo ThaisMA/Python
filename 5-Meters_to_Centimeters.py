5c2cb653c3e007d3dbb756c02263f74bd2e039ac
ThaisMA <23303802+ThaisMA@users.noreply.github.com>
2024-12-16 05:19:01 -0300

    Delete 5-Meters_to_Centimeters.py

--- 5-Meters_to_Centimeters.py
+++
@@ -1,7 +0,0 @@
-#Fifth Program
-#Make a Program that converts meters to centimeters.
-
-meters = (input("Enter the value in meters: "))
-centimeters = int(meters) * 100 
-
-print('{0} meter(s) is {1} centimeters'.format(meters,centimeters))
