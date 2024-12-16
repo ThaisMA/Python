274f4499dd1404337564de1cf203957f2af52077
ThaisMA <23303802+ThaisMA@users.noreply.github.com>
2024-12-16 05:19:25 -0300

    Delete 2-Show_The_Number.py

--- 2-Show_The_Number.py
+++
@@ -1,5 +0,0 @@
-#Second Program
-#Make a program that asks for a number and then displays the message The number entered was [number].
-
-number = (input("Enter a number: "))
-print("The number entered was: ",number)
