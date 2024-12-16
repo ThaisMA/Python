3cf85426d1f7620145b12982a991d43c36e2a081
ThaisMA <23303802+ThaisMA@users.noreply.github.com>
2024-12-16 05:18:36 -0300

    Delete 8-Salary_In_The_Month.py

--- 8-Salary_In_The_Month.py
+++
@@ -1,9 +0,0 @@
-#Eighth Exercise
-#Make a Program that asks how much you earn per hour and the number of 
-#hours worked in the month. Calculate and show your total salary in that month.
-
-salary = (input("Enter your salary per hour: "))
-hours = (input("Enter the number of hours worked: "))
-
-total = float(salary) * float(hours)
-print("Your Salary in that month is:{0}".format(total))
